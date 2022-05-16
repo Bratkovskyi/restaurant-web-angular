88efd5906a23306efe5f718190759e6f7144eb68
BratVlad <vlad.bratkovskiy@gmail.com>
2022-05-16 15:01:33 +0300

    menupages ready

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -11,7 +11,7 @@ export class OrderDetailsService {
       id: 1,
       foodName: 'Paneer Grilled Sandwich',
       foodDetails: 'Pan-fried masala paneer.',
-      foodPrice: 200,
+      foodPrice: 1.75,
       foodImg:
         'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq',
     },
@@ -20,7 +20,7 @@ export class OrderDetailsService {
       foodName: 'Veggie Supreme',
       foodDetails:
         'Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese',
-      foodPrice: 369,
+      foodPrice: 5.99,
       foodImg:
         'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k',
     },
@@ -28,7 +28,7 @@ export class OrderDetailsService {
       id: 3,
       foodName: 'Paneer Burger',
       foodDetails: 'panner',
-      foodPrice: 149,
+      foodPrice: 2.35,
       foodImg:
         'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k',
     },
@@ -37,7 +37,7 @@ export class OrderDetailsService {
       foodName: 'Veg Masala Roll In Naan',
       foodDetails:
         "A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
-      foodPrice: 140,
+      foodPrice: 6.49,
       foodImg:
         'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7',
     },
@@ -46,7 +46,7 @@ export class OrderDetailsService {
       foodName: 'Indulgence Brownie',
       foodDetails:
         '(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.',
-      foodPrice: 105,
+      foodPrice: 2.69,
       foodImg:
         'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv',
     },
@@ -54,7 +54,7 @@ export class OrderDetailsService {
       id: 6,
       foodName: 'Oreo Cheesecake Ice Cream',
       foodDetails: 'Oreo ice cream',
-      foodPrice: 219,
+      foodPrice: 1.15,
       foodImg:
         'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6',
     },
